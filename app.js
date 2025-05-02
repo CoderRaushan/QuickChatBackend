2bca68d1e56de20941ce6edcf287da7d57528f4c
CoderRaushan <raushankumar23082004@gmail.com>
2025-05-02 22:59:05 +0530

    also backend cors are added

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(urlencoded({extended:true}));
 const corsOptions={
-    origin:["http://localhost:5173"],
+    origin:["http://localhost:5173","https://quickchatbackend-etjb.onrender.com"],
     credentials:true,
 }
 
